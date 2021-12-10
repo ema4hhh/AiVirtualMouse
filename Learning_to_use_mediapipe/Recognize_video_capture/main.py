aca303650f99f43c5963e8852757b98f4b5f1e2c
ema4hhh <ema4hhh@gmail.com>
2021-12-10 10:23:12 -0500

    finished basic learning process

--- Learning_to_use_mediapipe/Recognize_video_capture/main.py
+++ Learning_to_use_mediapipe/Recognize_video_capture/main.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_V4L)
 
 with mp_hands.Hands(
   static_image_mode = False,
@@ -12,4 +12,28 @@ with mp_hands.Hands(
   min_detection_confidence = 0.5) as hands:
   
   while True:
+    ret, frame = cap.read()
+    if ret == False:
+      break
     
+    height, witdh, _ = frame.shape
+    frame = cv2.flip(frame, 1)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    results = hands.process(frame_rgb)
+    
+    if results.multi_hand_landmarks is not None:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+          frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+          mp_drawing.DrawingSpec(color=(128,117,128), thickness=2, circle_radius=3),
+          mp_drawing.DrawingSpec(color=(128, 0, 128), thickness=2)
+        )
+    
+    cv2.imshow("Frame", frame)
+    
+    if cv2.waitKey(1) & 0xFF == 27:
+      break
+    
+cap.release()
+cv2.destroyAllWindows()
